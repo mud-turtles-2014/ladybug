e85992c75b58c6f8e7703bacf64a1a841597dc9c
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-17 14:51:20 -0400

    Nested expenses under legs in trips form

--- app/assets/javascripts/trip.js
+++ app/assets/javascripts/trip.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
   console.log("ready")
 
-  $('#add-location').click(function(e){
-    e.preventDefault();
-    addLegForm();
-  })
+  $('form').on('click','.remove_fields', function(event){
+    event.preventDefault();
+    $(this).prev('input[type=hidden]').val('1');
+    $(this).closest('fieldset').hide();
+  });
+
+  $('form').on('click','.add_fields', function(event){
+    event.preventDefault();
+    time = new Date().getTime();
+    regexp = new RegExp($(this).data('id'), 'g');
+    $(this).before($(this).data('fields').replace(regexp, time));
+  });
+  // $('#add-location').click(function(e){
+  //   e.preventDefault();
+  //   addLegForm();
+  // })
 })
 
 
