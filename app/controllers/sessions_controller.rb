a9dd0b70f053b9ba8733ee0f9f18ffac80d2132f
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-10-17 12:06:21 -0400

    redirect on login to user homepage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
     if @user
       session[:user_id] = @user.id
-      redirect_to trips_path
+      redirect_to user_path(@user)
     else
       flash[:login] = "Invalid Credentials"
       redirect_to trips_path
